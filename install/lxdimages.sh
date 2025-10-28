1820c94ace6b8e305ed7f06cb5bc5a7ce168a55b
HRET <hret.dev@qq.com>
2025-10-28 21:15:27 +0800

    Update lxdimages.sh

--- install/lxdimages.sh
+++ install/lxdimages.sh
@@ -3,7 +3,7 @@
 RED='\033[0;31m'; GREEN='\033[0;32m'; YELLOW='\033[1;33m'
 BLUE='\033[0;34m'; NC='\033[0m'
 
-REPO="https://github.com/xkatld/zjmf-lxd-server"
+REPO="https://hubproxy.hret.dev/https://github.com/HRET-Dev/zjmf-lxd-server"
 NAME="lxdimages"
 INSTALL_DIR="/usr/local/bin"
 FORCE=false
